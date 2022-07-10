47304b12caa1439675c9f895a66b8af403b207bc
Vishnu-vashishth <vishnu.cse20@satyug.edu.in>
2022-07-10 08:08:10 +0530

    blog app

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>@Varsh Daily</h2>
+    <h1>
+      <span>“</span> Blogs <span>”</span>
+    </h1>
+    <p>
+      awesome place to make oneself <br /> productive and entertained through
+      daily updates.
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,100 @@
+export const blogList = [
+  {
+    id: 1,
+    title: '7 CSS tools you should be using ',
+    category: 'development',
+    subCategory: ['frontend', 'ui/ux', 'design'],
+    description:
+       `CSS has a lot of cool features that allow us to create amazing experiences on the web. By using this stylesheet language, you can make web pages beautiful and responsive on all devices. You can also create good-looking layouts with the power of flexbox and grid. There is a lot to do with CSS and it’s very useful for web development. However, writing and managing good CSS code is not that easy. Especially if you’re working on large projects and you’re not using a CSS framework. That’s why it would be useful to use some available CSS tools on the web to make your life easier.`,
+    authorName: 'Varsh',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'July 10, 2022',
+    cover: '/assets/images/designer-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'What is Customer Relationship Management (CRM) and Why You Need It',
+    category: 'crm',
+    subCategory: ['crm', 'marketing'],
+    description:`We can categorize customers and prospects in profitability tiers by linking them to demographic data and purchasing information. However, this is only an analytical view of clients, treating them as resources for our selling efforts. We can yield much better results if we treat them as human beings looking for genuine and more personalized interactions.
+
+    
+    As our business grows, keeping all our customer data in spreadsheets and shared folders becomes a task that is tedious and prone to error. Critical data is scattered into employees’ notepads, e-mail inboxes, and social media accounts.
+    
+    There is a lot to do with CSS and it’s very useful for web development. However, writing and managing good CSS code is not that easy. Especially if you’re working on large projects and you’re not using a CSS framework. 
+    That’s why it would be useful to use some available CSS tools on the web to make your life easier.
+    
+    Key features of a CRM
+
+    1.Contact management;
+    2.Lead and prospect management;
+    3.Sales forecasting;
+    4.Social media and e-mail integration;
+    5.Dashboard performance analytics.`,
+    authorName: 'varsh',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'July 10, 2022',
+    cover: '/assets/images/crm.jpeg',
+  },
+  {
+    id: 3,
+    title: 'Online Shopping – An Alternative to Shopping in the Mall',
+    category: 'shopping',
+    subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'varh',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/fQwuyKJ9qxjSbr6REcgtmW-1200-80.jpg',
+  },
+  {
+    id: 4,
+    title: 'ADVENTURE IN YOU',
+    category: 'adventure',
+    subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/graphic-design-trends.png',
+  },
+  // {
+  //   id: 5,
+  //   title: 'Loaded BBQ Baked Potatoes',
+  //   category: 'cooking',
+  //   subCategory: ['bbq', 'food', 'lifestyle'],
+  //   description:
+  //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  //   authorName: 'John Doe',
+  //   authorAvatar: '/assets/images/author.jpg',
+  //   createdAt: 'June 03, 2021',
+  //   cover: '/assets/images/make-it-personal.jpg',
+  // },
+  {
+    id: 6,
+    title: 'Beyond the Beach',
+    category: 'travel',
+    subCategory: ['beaches', 'sea', 'holidays'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover:
+      '/assets/images/Purple-Combination-colors-graphic-design-predictions-1024x576-1024x576.jpg',
+  },
+  {
+    id: 7,
+    title: 'Art & Perception',
+    category: 'art',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'John Doe',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/Synthwave-Postmodern.jpg',
+  },
+];
